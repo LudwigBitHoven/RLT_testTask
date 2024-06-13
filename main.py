a3b464505fee093a3501671a9fb3dd9a8cc0344d
Николай Чаптыков <nick.chaptykov@gmail.com>
2024-06-13 15:44:41 +0300

    Обновил комментарии к функциям классов

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class BaseAggregator:
     _logging = True
 
     async def run(self, input_json):
+        """
+            основная точка входа, может засекать время выполнения если _logging = True
+        """
         await self.validate_json()
         start = time.time()
         self._aggregate(input_json)
@@ -21,13 +24,16 @@ class BaseAggregator:
             print(f"Finished, took {end - start} seconds")
 
     async def validate_json(self, input_json):
+        # проверка типа
         if not isinstance(input_json, dict):
             raise ValueError("It's not a json")
+        # проверка ключей
         if not (
             input_json.get("dt_from") and 
             input_json.get("dt_upto") and 
             input_json.get("group_type")):
             raise KeyError("Not enough keys")
+        # проверка значений
         group_type_values = ["hour", "day", "week", "month", "year"]
         if not input_json["group_type"] in group_type_values:
             raise ValueError("No such group type")
@@ -36,7 +42,8 @@ class BaseAggregator:
             datetime.datetime.fromisoformat(input_json["dt_upto"])
         except Exception:
             raise ValueError("Something wrong with dates")
-        print("Validation completed successfully")
+        if BaseAggregator._logging:
+            print("Validation completed successfully")
 
 
 class Aggregator(BaseAggregator):
@@ -44,12 +51,21 @@ class Aggregator(BaseAggregator):
         pass
 
     async def generate_date_range(self, input_json):
+        """
+            генерирует даты с заданным в group_type шагом
+        """
         pass
 
     async def compose_pipeline(self, input_json):
+        """
+            составление пайплайна для агрегации
+        """
         pass
 
     async def glue_together(self, groups, date_range):
+        """
+            склеивание дат и результатов агрегации
+        """
         pass
 
 
