5b6c6b8f5ed16eda97e035e38df423298f5be5f5
Николай Чаптыков <nick.chaptykov@gmail.com>
2024-06-13 15:52:13 +0300

    Добавил генерацию дат с заданным шагом, исправил ошибку в BaseAggregator

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ class BaseAggregator:
         """
             основная точка входа, может засекать время выполнения если _logging = True
         """
-        await self.validate_json()
+        await self.validate_json(input_json)
         start = time.time()
-        self._aggregate(input_json)
+        await self._aggregate(input_json)
         end = time.time()
         if BaseAggregator._logging:
             print(f"Finished, took {end - start} seconds")
@@ -29,9 +29,9 @@ class BaseAggregator:
             raise ValueError("It's not a json")
         # проверка ключей
         if not (
-            input_json.get("dt_from") and 
-            input_json.get("dt_upto") and 
-            input_json.get("group_type")):
+                input_json.get("dt_from") and
+                input_json.get("dt_upto") and
+                input_json.get("group_type")):
             raise KeyError("Not enough keys")
         # проверка значений
         group_type_values = ["hour", "day", "week", "month", "year"]
@@ -48,13 +48,23 @@ class BaseAggregator:
 
 class Aggregator(BaseAggregator):
     async def _aggregate(self, input_json):
-        pass
+        # перевевдем string в datetime
+        input_json["dt_from"] = datetime.datetime.fromisoformat(input_json["dt_from"])
+        input_json["dt_upto"] = datetime.datetime.fromisoformat(input_json["dt_upto"])
+        date_range = await self.generate_date_range(input_json)
+        print(date_range)
 
     async def generate_date_range(self, input_json):
         """
-            генерирует даты с заданным в group_type шагом
+            генерирует даты с заданным в group_type шагом в пределе от dt_from до dt_upto
         """
-        pass
+        group_type = {input_json["group_type"] + "s": 1}
+        dt_from = input_json["dt_from"]
+        range_dict = {}
+        while dt_from <= input_json["dt_upto"]:
+            range_dict[dt_from.isoformat()] = 0
+            dt_from += datetime.timedelta(**group_type)
+        return range_dict
 
     async def compose_pipeline(self, input_json):
         """
@@ -70,9 +80,9 @@ class Aggregator(BaseAggregator):
 
 
 test = {
-   "dt_from": "2022-02-01T00:00:00",
-   "dt_upto": "2022-02-02T00:00:00",
-   "group_type": "hour"
+    "dt_from": "2022-02-01T00:00:00",
+    "dt_upto": "2022-02-02T00:00:00",
+    "group_type": "hour"
 }
 
 temp = Aggregator()
